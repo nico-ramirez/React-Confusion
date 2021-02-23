c57f3af5285c8db2c9484f305afdca3dddf5cfff
Nicolas <nicolas.ramirez.28@hotmail.com>
2021-02-23 18:57:03 -0300

    Header and Footer

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,28 +1,25 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay,
-    CardTitle } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
-    function RenderMenuItem ({dish, onClick}) {
-        return (
-            <Card
-                onClick={() => onClick(dish.id)}>
+    function RenderMenuItem({ dish, onClick }) {
+        return(
+            <Card onClick={() => onClick(dish.id)}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
+                </CardImgOverlay> 
             </Card>
         );
     }
 
     const Menu = (props) => {
-
         const menu = props.dishes.map((dish) => {
             return (
-                <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <RenderMenuItem dish={dish} onClick={props.onClick}/>
                 </div>
             );
-        });
+        })
 
         return (
             <div className="container">
@@ -31,6 +28,7 @@ import { Card, CardImg, CardImgOverlay,
                 </div>
             </div>
         );
-    }
 
+    }
+       
 export default Menu;
